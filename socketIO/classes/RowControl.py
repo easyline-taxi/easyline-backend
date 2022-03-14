f1805b69f4c2bca2db39b03fb029c810c90043b2
Wesley <wesleybenicio4@gmail.com>
2022-03-14 15:02:04 -0300

    Remove limit connections

--- socketIO/classes/RowControl.py
+++ socketIO/classes/RowControl.py
@@ -16,8 +16,6 @@ class RowControl(socketio.Namespace):
         if res and res.is_valid():            
             if res.user.point_id and res.user.point_id > 0:
                 user ,exist = models.ClientWebsocket.objects.get_or_create(user=res.user,sid=sid,point=res.user.point_id)
-                if not exist:
-                    user.delete()
                 models.HistoricWebsocket.objects.create(user=res.user,sid=sid,point=res.user.point_id,action="connected websocket")
                 self.save_session(sid, {'user': res.user,'point':res.user.point_id})
                 self.enter_room(sid, res.user.point_id)
@@ -34,14 +32,17 @@ class RowControl(socketio.Namespace):
         
         
     def on_set_location(self, sid, data):
-        session = self.get_session(sid)
-        models.HistoricWebsocket.objects.create(user=session.get("user"),sid=sid,point=session.get("point"),action="send set_location",complement=data)
-        res = SET_LOCALE(session.get("user"),data)
-        if res.get("response").get("update"):
-            models.HistoricWebsocket.objects.create(user=session.get("user"),sid=sid,point=session.get("point"),action="receive row_update",complement=data)
-            self.emit('row_update', {"status": 200, "data": res},room=session.get("point"))
-        models.HistoricWebsocket.objects.create(user=session.get("user"),sid=sid,point=session.get("point"),action="receive location_updated",complement=data)
-        self.emit('location_updated', {"status": 200, "data": res})
+        try:
+            session = self.get_session(sid)
+            models.HistoricWebsocket.objects.create(user=session.get("user"),sid=sid,point=session.get("point"),action="send set_location",complement=data)
+            res = SET_LOCALE(session.get("user"),data)
+            if res.get("response").get("update"):
+                models.HistoricWebsocket.objects.create(user=session.get("user"),sid=sid,point=session.get("point"),action="receive row_update",complement=data)
+                self.emit('row_update', {"status": 200, "data": res},room=session.get("point"))
+            models.HistoricWebsocket.objects.create(user=session.get("user"),sid=sid,point=session.get("point"),action="receive location_updated",complement=data)
+            self.emit('location_updated', {"status": 200, "data": res})
+        except Exception as ex:
+            models.HistoricWebsocket.objects.create(user=session.get("user"),sid=sid,point=session.get("point"),action=str(ex))
 
     def on_disconnect(self, sid):
         print('disconnect ', sid)
